02c679ec7f67b4424a7a736e6470c8f667da1d52
Alex Bondarenko <alexandr96b@gmail.com>
2019-05-23 16:26:03 +0300

    add page contacts

--- js/script.js
+++ js/script.js
@@ -227,6 +227,94 @@ $('.b-card-slider__prev').on("click", function() {
     $('.b-card-slider__big').slick("slickPrev"); 
 })
 
+// google maps
+
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 13,
+        disableDefaultUI: true,
+
+        // The latitude and longitude to center the map (always required)
+
+        center: new google.maps.LatLng(56.00754747, 92.86687374), // New York
+
+        // How you would like to style the map. 
+        // This is where you would paste any style found on Snazzy Maps.
+        styles: [
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#C6E2FF"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#C5E3BF"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#D1D1B8"
+            }
+        ]
+    }
+]
+    };
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using our element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({ 
+        position: new google.maps.LatLng(56.00754747, 92.86687374),
+        map: map,
+        title: 'Snazzy!',
+                icon: {
+                    url: "images/pin.svg",
+                    scaledSize: new google.maps.Size(57, 68),
+                    labelOrigin: new google.maps.Point(145, 25, 5),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(36, 49)           
+                }      
+
+    });
+
+}
 
 
 
@@ -544,62 +632,6 @@ $.extend( $.validator.messages, {
 
 
  
-// google maps
-
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 13,
-        disableDefaultUI: true,
-
-        // The latitude and longitude to center the map (always required)
-
-        center: new google.maps.LatLng(57.923140, 56.123019), // New York
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#dbdbdb"},{"lightness":2}]},
-        {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#dbdbdb"},{"lightness":1}]},
-        {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":2}]},
-        {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":2},{"weight":0.2}]},
-        {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":2}]},
-        {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":2}]},
-        {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":2}]},
-        {"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#777777"},{"lightness":0.5}]},{"elementType":"labels.text.fill",
-        "stylers":[{"saturation":1},{"color":"#777777"},{"lightness":1}]},{"elementType":"labels.icon",
-        "stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry",
-        "stylers":[{"color":"#ffffff"},{"lightness":1}]},
-        {"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":17},{"weight":1.2}]}]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({ 
-        position: new google.maps.LatLng(57.920978, 56.151657),
-        map: map,
-        title: 'Snazzy!',
-                icon: {
-                    url: "images/map.svg",
-                    scaledSize: new google.maps.Size(28, 40),
-                    labelOrigin: new google.maps.Point(145, 25, 5),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(36, 49)           
-                }      
-
-    });
-
-}
 
 
 
